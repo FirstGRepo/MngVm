1c83d6193e66a0623925eb8dffd27a9bcc2b3f59
Mohit Amichand <mohitamichand287@gmail.com>
2021-03-25 13:03:00 +0530

    CR::Mohit:: AzureSheet2

--- Constant/KeyConstant.cs
+++ Constant/KeyConstant.cs
@@ -35,4 +35,29 @@ namespace MngVm.Constant
         public string CostDiffColumn => ConfigurationManager.AppSettings["azvmServerCostDiffColumn"];
         public string ApiVersion => ConfigurationManager.AppSettings["azvmApiVersion"];
     }
+
+    public class AzureVMLoggerSheet2
+    {
+        public string VMLogSpreadSheetID => ConfigurationManager.AppSettings["azvmVMLogSpreadSheetID"];
+        public string VMLogSheetName => ConfigurationManager.AppSettings["azvmVMLogSheetName"];
+        public string AutoShutTime => ConfigurationManager.AppSettings["azvmAutoShutTime"];
+        public string LastSheetUpdated => ConfigurationManager.AppSettings["azvmLastSheetUpdated"];
+
+        public string ResourceGroupColumn => ConfigurationManager.AppSettings["azvmResourceGroupColumn"];
+        public string UserActiveColumn => ConfigurationManager.AppSettings["azvmUserActiveColumn"];
+        public string UserActiveDateTimeColumn => ConfigurationManager.AppSettings["azvmUserActiveDateTimeColumn"];
+        public string ServerColumn => ConfigurationManager.AppSettings["azvmServerColumn"];
+        public string ServerStatusColumn => ConfigurationManager.AppSettings["azvmServerStatusColumn"];
+        public string ServerDateTimeColumn => ConfigurationManager.AppSettings["azvmServerDateTimeColumn"];
+        public string RowIndex => ConfigurationManager.AppSettings["azvmRowIndex"];
+        public string CPU1Column => ConfigurationManager.AppSettings["azvmServerCPU1Column"];
+        public string CPU2Column => ConfigurationManager.AppSettings["azvmServerCPU2Column"];
+        public string CPU3Column => ConfigurationManager.AppSettings["azvmServerCPU3Column"];
+        public string CPU4Column => ConfigurationManager.AppSettings["azvmServerCPU4Column"];
+        public string CPUPeakColumn => ConfigurationManager.AppSettings["azvmServerCPUPeakColumn"];
+        public string ThisMonthCostColumn => ConfigurationManager.AppSettings["azvmServerThisMonthCostColumn"];
+        public string LastMonthCostColumn => ConfigurationManager.AppSettings["azvmServerLastMonthCostColumn"];
+        public string CostDiffColumn => ConfigurationManager.AppSettings["azvmServerCostDiffColumn"];
+        public string ApiVersion => ConfigurationManager.AppSettings["azvmApiVersion"];
+    }
 }
